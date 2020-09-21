b0ab4b99fa0f2124039a12dd028d574853129a6c
cpdf98 <cpdf98@yahoo.com>
2020-09-21 12:40:03 -0500

    added basic form

--- project/main.java
+++ project/main.java
@@ -1,9 +1,15 @@
 import java.util.*;
 import java.text.*;
 import java.io.*;
+import javax.swing.*;
 public class main{
 	public static DecimalFormat timeFormat = new DecimalFormat("#.##");
 	public static void main(String[] args) throws IOException{
+		/*JFrame frame = new JFrame("My First GUI");
+       		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       		frame.setSize(300,300);
+       		frame.setVisible(true);
+		*/
 		String[] scrambleAlg = scramble.guide();
 		double time = timer.guide(scrambleAlg);
 		System.out.println("Time: " + timeFormat.format(time));
