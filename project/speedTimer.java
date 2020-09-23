0ab97155bb082c5a1fffff6aee347236dba35463
cpdf98 <cpdf98@yahoo.com>
2020-09-23 16:21:56 -0500

    Renamed main class to speedTimer class. Replaced label with text field

--- project/speedTimer.java
+++ project/speedTimer.java
@@ -5,28 +5,25 @@ import javax.swing.*;
 import javax.swing.Timer;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemListener;
-public class main extends javax.swing.JFrame{
+public class speedTimer extends javax.swing.JFrame{
 	public static DecimalFormat timeFormat = new DecimalFormat("#.##");
-	public main(){
+	public speedTimer(){
 		initComponents();
 	}
 	    @SuppressWarnings("unchecked")
     	private void initComponents() {
 
         	scrambleLabel = new javax.swing.JLabel();
-        	timeLabel = new javax.swing.JLabel();
         	actionButton = new javax.swing.JButton();
+        	timeTextBox = new javax.swing.JTextField();
 
         	setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         	scrambleLabel.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
         	scrambleLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        	scrambleLabel.setText("Scramble");
+        	scramble newScramble = new scramble();
+		scrambleLabel.setText(Arrays.toString(newScramble.guide()));
         	scrambleLabel.setVerticalAlignment(javax.swing.SwingConstants.TOP);
-
-        	timeLabel.setFont(new java.awt.Font("Dialog", 1, 48)); // NOI18N
-        	timeLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        	timeLabel.setText("0.00");
 
         	actionButton.setText("Start");
         	actionButton.addActionListener(new java.awt.event.ActionListener() {
@@ -34,6 +31,10 @@ public class main extends javax.swing.JFrame{
                 		actionButtonActionPerformed(evt);
             		}
         	});
+
+        	timeTextBox.setFont(new java.awt.Font("Dialog", 0, 36)); // NOI18N
+        	timeTextBox.setText("0.00");
+		timeTextBox.setEnabled(false);
 
         	javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         	getContentPane().setLayout(layout);
@@ -44,10 +45,13 @@ public class main extends javax.swing.JFrame{
                 		.addComponent(scrambleLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 986, javax.swing.GroupLayout.PREFERRED_SIZE)
                 		.addGap(92, 92, 92))
             		.addGroup(layout.createSequentialGroup()
-                		.addGap(445, 445, 445)
                 		.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    			.addComponent(actionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    			.addComponent(timeLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    			.addGroup(layout.createSequentialGroup()
+                        			.addGap(445, 445, 445)
+                        			.addComponent(actionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    			.addGroup(layout.createSequentialGroup()
+                        			.addGap(532, 532, 532)
+                        			.addComponent(timeTextBox, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE))) //controls width of text box
                 		.addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         	);
         	layout.setVerticalGroup(
@@ -55,57 +59,53 @@ public class main extends javax.swing.JFrame{
             		.addGroup(layout.createSequentialGroup()
                 		.addGap(45, 45, 45)
                 		.addComponent(scrambleLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
-                		.addGap(49, 49, 49)
-                		.addComponent(timeLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                		.addGap(30, 30, 30)
+                		.addComponent(timeTextBox, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
                 		.addGap(56, 56, 56)
                 		.addComponent(actionButton, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
                 		.addContainerGap(453, Short.MAX_VALUE))
         	);
 
-        	timeLabel.getAccessibleContext().setAccessibleName("0.00");
-
         	pack();
-    	}
+    	}                                  	
 	private void actionButtonActionPerformed(java.awt.event.ActionEvent evt){
 		//scramble newScramble = new scramble();
 		//scrambleLabel.setText(Arrays.toString(newScramble.guide()));
-		//timer newTime = new timer();
 		stopWatch();
 	}
-	private int secondsToWait = 10;
+	private double secondsToWait = 10;
 	private Timer timer;
 	public void stopWatch(){
-		timer = new Timer(secondsToWait, e -> {
-			timeLabel.setText((secondsToWait/1000) + "");
-				secondsToWait += 1000;
+		timer = new Timer((int)secondsToWait, e -> {
+			timeTextBox.setText((secondsToWait/1000) + "");
+				secondsToWait += 10;
 			});
 			timer.start();
 	}
 	public static void main(String[] args){
-        	try{
+		try {
             		for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                		if ("Nimbus".equals(info.getName())){
+                		if ("Nimbus".equals(info.getName())) {
                     			javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     			break;
                 		}
             		}
 		}catch(ClassNotFoundException ex){
-            		java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            		java.util.logging.Logger.getLogger(speedTimer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         	}catch(InstantiationException ex){
-            		java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            		java.util.logging.Logger.getLogger(speedTimer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         	}catch(IllegalAccessException ex){
-            		java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            		java.util.logging.Logger.getLogger(speedTimer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         	}catch(javax.swing.UnsupportedLookAndFeelException ex){
-            		java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            		java.util.logging.Logger.getLogger(speedTimer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         	}
-
-        	java.awt.EventQueue.invokeLater(new Runnable() {
+        	java.awt.EventQueue.invokeLater(new Runnable(){
             		public void run(){
-                		new main().setVisible(true);
+                		new speedTimer().setVisible(true);
             		}
         	});
-	}
-	private javax.swing.JButton actionButton;
+    	}
+    	private javax.swing.JButton actionButton;
     	private javax.swing.JLabel scrambleLabel;
-    	private javax.swing.JLabel timeLabel;
+    	private javax.swing.JTextField timeTextBox;
 }
